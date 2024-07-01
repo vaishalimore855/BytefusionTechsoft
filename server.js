eb02814db424809932439098be5201efc694710b
vaishalimore855 <111679670+vaishalimore855@users.noreply.github.com>
2024-07-01 17:09:59 +0530

    Making the Better API
    1.Sorting
    2.Advanced Filtering
    3.Alising
    4.Pagination

--- server.js
+++ server.js
@@ -41,6 +41,30 @@
 /*************************Making Better API:Filtering */
 // server.js
 
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const tourRouter = require('./routes/tourRoutes');
+
+// const app = express();
+
+// app.use(express.json());
+
+// const DB = 'mongodb://localhost:27017/tourDB';
+// mongoose.connect(DB, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => console.log('DB connection successful!'));
+
+// app.use('/api/v1/tours', tourRouter);
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
+
+//Making the better API:Advanced filtering,Sorting,Pagination,Aliasing
+// app.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const tourRouter = require('./routes/tourRoutes');
--- tourController.js
+++ tourController.js
@@ -1,6 +1,77 @@
+// // controllers/tourController.js
+
+// const Tour = require('../models/tourModel');
+
+// exports.getAllTours = async (req, res) => {
+//   try {
+//     // Build query
+//     const queryObj = { ...req.query };
+//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//     excludedFields.forEach(el => delete queryObj[el]);
+
+//     // Advanced filtering
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+//     let query = Tour.find(JSON.parse(queryStr));
+
+//     // Sorting
+//     if (req.query.sort) {
+//       const sortBy = req.query.sort.split(',').join(' ');
+//       query = query.sort(sortBy);
+//     } else {
+//       query = query.sort('-createdAt');
+//     }
+
+//     // Field limiting
+//     if (req.query.fields) {
+//       const fields = req.query.fields.split(',').join(' ');
+//       query = query.select(fields);
+//     } else {
+//       query = query.select('-__v');
+//     }
+
+//     // Pagination
+//     const page = req.query.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+//     const skip = (page - 1) * limit;
+//     query = query.skip(skip).limit(limit);
+
+//     if (req.query.page) {
+//       const numTours = await Tour.countDocuments();
+//       if (skip >= numTours) throw new Error('This page does not exist');
+//     }
+
+//     // Execute query
+//     const tours = await query;
+
+//     // Send response
+//     res.status(200).json({
+//       status: 'success',
+//       results: tours.length,
+//       data: {
+//         tours
+//       }
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       status: 'fail',
+//       message: err.message
+//     });
+//   }
+// };
+
+
+
 // controllers/tourController.js
 
 const Tour = require('../models/tourModel');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
@@ -59,3 +130,4 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
+
