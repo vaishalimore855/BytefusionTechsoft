eac9646799d22075d5150120345c522113654bf2
vaishalimore855 <111679670+vaishalimore855@users.noreply.github.com>
2024-07-01 07:45:50 +0530

    connecting our Database with the Express

--- app.js
+++ app.js
@@ -1,69 +1,17 @@
+// const app= express = require("express");
+// const http= require('http').Server(app);
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb+srv://vaishalimore855:Vaishali@123@temp-test.5ihydqf.mongodb.net/?retryWrites=true&w=majority&appName=Temp-Test");
+// const User = require("./modles/userModule");
 
-/************************ Building the simple API******************** */
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/api', (req, res) => {
-//   res.json({ message: 'Hello, this is your API!' });
+// async function insert()
+// {
+//     await User.create({
+//         name:"Vaishali",
+//         email:"vaishalimore@gmail.com"
+//     })
+// };
+// insert();
+// http.listen(3000,function(){
+//     console.log("Server is Running")
 // });
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not Found' });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}/`);
-// });
-/******************************************************************* */
-
-
-
-
-
-/*************************************using third party module- lodash */
-// app.js
-const _ = require('lodash');
-
-// Example usage of lodash functions
-const numbers = [10, 5, 100, 2, 1000];
-const sortedNumbers = _.sortBy(numbers);
-console.log('Sorted Numbers:', sortedNumbers);
-
-const users = [
-  { user: 'fred', age: 48 },
-  { user: 'barney', age: 34 },
-  { user: 'fred', age: 40 },
-  { user: 'barney', age: 36 }
-];
-const groupedUsers = _.groupBy(users, 'user');
-console.log('Grouped Users:', groupedUsers);
-/************************************************************************* */
-
-const express = require('express');
-
-// Create an Express application
-const app = express();
-
-// Define a simple route for the home page
-app.get('/', (req, res) => {
-    res.send('Welcome to my Express app!');
-});
-
-// Define a route for the about page
-app.get('/about', (req, res) => {
-    res.send('This is the about page.');
-});
-
-// Define a route for the contact page
-app.get('/contact', (req, res) => {
-    res.send('This is the contact page.');
-});
-
-// Define a port to listen on
-const port = 3000;
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
