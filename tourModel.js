58c72548f25005bac8a854a52fb606617f44d69d
vaishalimore855 <111679670+vaishalimore855@users.noreply.github.com>
2024-07-02 07:53:15 +0530

    Add Virtual Properties

--- tourModel.js
+++ tourModel.js
@@ -1,12 +1,38 @@
+/*********************** Virtual Properties****************** */
 // models/tourModel.js
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-  name: String,
-  duration: Number,
-  difficulty: String,
-  price: Number,
-  ratingsAverage: Number,
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+// Adding a virtual property 'durationWeeks'
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
