affe1332ec3ef26738e3bad38697aee98430a822
vaishalimore855 <111679670+vaishalimore855@users.noreply.github.com>
2024-07-02 06:52:59 +0530

    Refactring API Features

--- tourController.js
+++ tourController.js
@@ -62,9 +62,78 @@
 
 
 
+// // controllers/tourController.js
+// //Making the Better API:Advanced
+// const Tour = require('../models/tourModel');
+
+// exports.aliasTopTours = (req, res, next) => {
+//   req.query.limit = '5';
+//   req.query.sort = '-ratingsAverage,price';
+//   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+//   next();
+// };
+
+// exports.getAllTours = async (req, res) => {
+//   try {
+//     // Build query
+//     const queryObj = { ...req.query };
+//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//     excludedFields.forEach(el => delete queryObj[el]);
+
+//     // Advanced filtering
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+//     let query = Tour.find(JSON.parse(queryStr));
+
+//     // Sorting
+//     if (req.query.sort) {
+//       const sortBy = req.query.sort.split(',').join(' ');
+//       query = query.sort(sortBy);
+//     } else {
+//       query = query.sort('-createdAt');
+//     }
+
+//     // Field limiting
+//     if (req.query.fields) {
+//       const fields = req.query.fields.split(',').join(' ');
+//       query = query.select(fields);
+//     } else {
+//       query = query.select('-__v');
+//     }
+
+//     // Pagination
+//     const page = req.query.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+//     const skip = (page - 1) * limit;
+//     query = query.skip(skip).limit(limit);
+
+//     if (req.query.page) {
+//       const numTours = await Tour.countDocuments();
+//       if (skip >= numTours) throw new Error('This page does not exist');
+//     }
+
+//     // Execute query
+//     const tours = await query;
+
+//     // Send response
+//     res.status(200).json({
+//       status: 'success',
+//       results: tours.length,
+//       data: {
+//         tours
+//       }
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       status: 'fail',
+//       message: err.message
+//     });
+//   }
+// };
 // controllers/tourController.js
 
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utills/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -75,47 +144,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Build query
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    // Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
-    // Execute query
-    const tours = await query;
-
-    // Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -130,4 +165,5 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
+
 
