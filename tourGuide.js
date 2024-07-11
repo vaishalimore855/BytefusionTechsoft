aec4a0d4bda6ad917b3d3521701fb7176b116bf1
vaishalimore855 <111679670+vaishalimore855@users.noreply.github.com>
2024-07-11 16:57:54 +0530

    Creating and Getting Reviews

--- tourGuide.js
+++ tourGuide.js
@@ -1,25 +1,9 @@
 const mongoose = require('mongoose');
 
-// Define the Tour schema
-const tourSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    duration: { type: Number, required: true }, // in hours
-    price: { type: Number, required: true },
-    location: {
-        type: { type: String, enum: ['Point'], required: true },
-        coordinates: { type: [Number], required: true }
-    },
-    date: { type: Date, required: true }
-});
-
-// Define the TourGuide schema with embedded Tour documents
 const tourGuideSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
-    tours: [tourSchema] // Embedding tours
+    tours: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tour' }]
 });
-
-// Create a 2dsphere index on the location field of tours
-tourSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('TourGuide', tourGuideSchema);
